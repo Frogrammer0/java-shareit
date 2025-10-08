591f53a5e27afbd7033c67ce069835b7e9f4cfea
Roman Golubev <roman222golubev@mail.ru>
2025-10-08 16:28:17 +0300

    выполнить ФЗ14

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.InMemoryUserStorage;
